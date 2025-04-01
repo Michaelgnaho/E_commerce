e34bd63eb1d48da60c167026dc528989a994a0d0
Michaelgnaho <gnahomiael@gmail.com>
2025-04-01 07:58:59 +0100

    update

--- config/ingest.js
+++ config/ingest.js
@@ -26,3 +26,39 @@ export const syncUserCreation = inngest.createFunction(
     return event;
   }
 );
+
+// inngest function to handle user update
+export const syncUserUpdate = inngest.createFunction(
+  { id: "sync-user-update-from-clerk" },
+  { event: "clerk/user.updated" },
+  async (event) => {
+    // Handle the event here
+    const { id, first_name, email_addresses, last_name, imageUrl } = event.data;
+    const userData = {
+      _id: id,
+      name: `${first_name} ${last_name}`,
+      email: email_addresses[0].email_address,
+      imgUrl: imageUrl,
+    };
+
+    await ConnectDB();
+    await User.findByIdAndUpdate(id, userData);
+    console.log("Event received:", event);
+    return event;
+  }
+);
+
+// inngest function to handle user deletion
+export const syncUserDeletion = inngest.createFunction(
+  { id: "sync-user-deletion-from-clerk" },
+  { event: "clerk/user.deleted" },
+  async (event) => {
+    // Handle the event here
+    const { id } = event.data;
+
+    await ConnectDB();
+    await User.findByIdAndDelete(id);
+    console.log("Event received:", event);
+    return event;
+  }
+);
